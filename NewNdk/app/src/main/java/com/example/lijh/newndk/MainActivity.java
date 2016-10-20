9cf59ba78f13c89b38e5c36614ea19cc19cf8264
lijh <jh_shigang@126.com>
2016-10-20 15:12:04 +0800

    Nothing

--- NewNdk/app/src/main/java/com/example/lijh/newndk/MainActivity.java
+++ NewNdk/app/src/main/java/com/example/lijh/newndk/MainActivity.java
@@ -6,35 +6,20 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-
     private TextView mTextView;
 
-
-
     static {
-
         System.loadLibrary("newndk");
-
     }
-
-
 
     public native String hello();
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-
-
         mTextView= (TextView) findViewById(R.id.main_textview);
-
         mTextView.setText(hello()+"");
-
     }
 }
